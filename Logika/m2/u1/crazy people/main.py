1a69cf9079edef35ebfda11a1efeda57acd64f7e
Vicktoriya14 <134728131+Vicktoriya14@users.noreply.github.com>
2023-08-03 19:42:25 +0300

    Add files via upload

---
+++ Logika/m2/u1/crazy people/main.py
@@ -0,0 +1,27 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+from random import randint
+
+app = QApplication([])
+main_window = QWidget()
+
+button = QPushButton('Згенерувати')
+text = QLabel('Натисни щоб дізнатися переможця')
+winner = QLabel('?')
+
+line = QVBoxLayout()
+line.addWidget(text)
+line.addWidget(winner)
+line.addWidget(button)
+
+def win():
+    ran = randint(1, 1000)
+    winner.setText(str(ran))
+
+button.clicked.connect(win)
+
+
+main_window.setLayout(line)
+
+main_window.show()
+app.exec_()
