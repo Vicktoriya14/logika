4aa0ba25876d6022a40b50500ea946795299774e
Vicktoriya14 <134728131+Vicktoriya14@users.noreply.github.com>
2023-09-19 21:27:29 +0300

    Add files via upload

--- Logika/m3/u1/основи/main1.py
+++ Logika/m3/u1/основи/main1.py
@@ -15,3 +15,15 @@ with open('quotes1.txt', 'r', encoding='utf-8') as file:
 for student in students:
     if student.grade == 5:
         print(student.surname)
+
+
+amount = len(students)
+print (amount)
+
+bal = 0
+for line in students:
+    bal = bal + line.grade
+print(bal)
+
+x = bal/amount
+print(x)
